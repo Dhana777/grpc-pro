5446a4776e10cc6142243f9fdebf00721d9e99d2
cvdhanalakshmi <c_vdhanalakshmi@cloudbees.com>
2024-02-06 15:20:20 +0530

    commit

--- greet/client/main.go
+++ greet/client/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
+	"io"
 	"log"
+	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -9,7 +12,7 @@ import (
 	pb "github.com/Dhana777/grpc-pro/greet/proto"
 )
 
-var addr string = "0.0.0.0:50001"
+var addr string = "0.0.0.0:50051"
 
 func main() {
 
@@ -19,8 +22,69 @@ func main() {
 	}
 
 	c := pb.NewGreetServiceClient(conn)
-	DoGreet(c)
+
+	// doGreet(c)
+	//	doGreetMany(c)
+	doLongGreet(c)
 
 	defer conn.Close()
 
+}
+
+func doGreet(c pb.GreetServiceClient) {
+
+	log.Println("DoGreet was invoked")
+
+	res, err := c.Greet(context.Background(), &pb.GreetRequest{
+		FirstName: "dhana",
+	})
+
+	if err != nil {
+		log.Fatalf("could not greet %v\n", err)
+	}
+
+	log.Printf("Greeting %s\n", res.Result)
+}
+
+func doGreetMany(c pb.GreetServiceClient) {
+	log.Println("Do Greet many times was invoked")
+
+	Stream, err := c.GreetManyTimes(context.Background(), &pb.GreetRequest{
+		FirstName: "dhana",
+	})
+
+	if err != nil {
+		log.Fatalf("could not greet %v\n", err)
+	}
+	for {
+		result, err := Stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error while reading the stream %v\n", err)
+		}
+		log.Printf("printing the result %+v", result.Result)
+	}
+
+}
+
+func doLongGreet(c pb.GreetServiceClient) {
+	log.Println("Do Greet many times was invoked")
+
+	Stream, err := c.LongGreet(context.Background())
+	if err != nil {
+		log.Fatalf("could not greet %v\n", err)
+	}
+	names := []string{"dhana", "chitty"}
+	for i := 0; i <= len(names)-1; i++ {
+		Stream.Send(&pb.GreetRequest{FirstName: names[i]})
+		time.Sleep(1 * time.Second)
+	}
+
+	res, err := Stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("error while receving response %v", err)
+	}
+	log.Printf("result %v", res)
 }
